251d9da4066a5a2b559c788363f3c11ead02094f
carlos fernandes <fercael@outlook.com>
2023-02-09 18:13:42 -0300

    Modify alinhamento do Form

--- src/components/ContactForm/styles.ts
+++ src/components/ContactForm/styles.ts
@@ -3,14 +3,20 @@ import styled from 'styled-components';
 export const Container = styled.main`
   width: 1216px;
   min-height: 2rem;
+  max-height: 616px;
   
   display: flex;
-  justify-content: flex-end;
+  justify-content: center;
+  gap: 2rem;
+
+  overflow: hidden;
 
   @media ( max-width: 1216px){
     max-width: 768px;
+    > img {
+      display: none;
+    }
   }
   @media ( max-width: 768px){
     max-width: 375px;
-  }
-`;
+  }`;
